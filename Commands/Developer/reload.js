f31f6ea27ac74290a8b5e6fb746a4c758eadd9e8
tyyyyyyyyyyyyyy <104456765+tyyyyyyyyyyyyyy@users.noreply.github.com>
2023-02-13 21:41:42 +0000

    Added a reload command!
    Update commands and events without having to restart the bot.

---
+++ Commands/Developer/reload.js
@@ -0,0 +1,49 @@
+const { ChatInputCommandInteraction, SlashCommandBuilder, EmbedBuilder, PermissionFlagsBits, Client } = require("discord.js");
+const { loadCommands } = require("../../Handlers/commandHandler");
+const { loadEvents } = require("../../Handlers/eventHandler");
+
+module.exports= {
+    developer: true,
+    data: new SlashCommandBuilder()
+    .setName("reload")
+    .setDescription("Reload the commands/events.")
+    .setDefaultMemberPermissions(PermissionFlagsBits.Administrator)
+    .addSubcommand((options) => options
+    .setName("events")
+    .setDescription("Reload your events")) 
+    .addSubcommand((options) => options
+    .setName("commands")
+    .setDescription("Reload your commands"))
+    .addSubcommand((options) => options
+    .setName("everything")
+    .setDescription("Reload commands and events")),
+    /**
+     * @param {ChatInputCommandInteraction} interaction
+     * @param {Client} client
+     */
+    execute(interaction, client){
+        const subcommand = interaction.options.getSubcommand();
+
+        switch (subcommand) {
+            case "events" : {
+                for(const [key, value] of client.events)
+                client.removeListener(`${key}`, value, true);
+                loadEvents(client)
+                interaction.reply({ content: "Reloaded Events", ephemeral: true });
+            }
+            break;
+            case "commands" : {
+                loadCommands(client);
+                interaction.reply({ content: "Reloaded Commands!", ephemeral: true})
+            }
+            break;
+            case "everything" : {
+                for(const [key, value] of client.events)
+                client.removeListener(`${key}`, value, true);
+                loadEvents(client)
+                loadCommands(client);
+                interaction.reply({ content: "Reloaded commands and events!", ephemeral: true });
+            }
+        }
+    }
+}
